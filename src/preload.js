167a21d638ced4fbec471cd026902e2e1ae17dcc
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-07-31 22:38:59 +0900

    Delete src directory

--- src/preload.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", async () => {
-	globalThis.addonApp = true;
-	fetch("http://eita.html.xdomain.jp/hcq/addon/code.js", {
-		cache: "no-store",
-	})
-		.then((n) => n.text())
-		.then(eval);
-});
